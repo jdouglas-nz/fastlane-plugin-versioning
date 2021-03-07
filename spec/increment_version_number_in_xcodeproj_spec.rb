14878ee92e69522e8573436fecebfe2b2a8699f1
John Douglas <john.douglas.nz@gmail.com>
2021-03-07 17:17:53 +1300

    whoops. moves test into correct spec file

--- spec/increment_version_number_in_xcodeproj_spec.rb
+++ spec/increment_version_number_in_xcodeproj_spec.rb
@@ -94,6 +94,49 @@ describe Fastlane::Actions::IncrementVersionNumberInXcodeprojAction do
       expect(current_target_version).to eq("1.0.0")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("1.0.0")
     end
+
+
+    it "should not replace Xcode's annotations with generic ones" do
+      xcodeproj_path = '/tmp/fastlane/tests/fastlane/xcodeproj/Test.xcodeproj'
+
+      pbxproj_path = File.join(xcodeproj_path, "project.pbxproj")
+
+      file = Fastlane::FastFile.new.parse("
+        lane :bump do
+          increment_version_number_in_xcodeproj(
+            xcodeproj: '#{xcodeproj_path}',
+            scheme: 'Dev Test',
+            bump_type: 'minor'
+          )
+        end")
+
+      pbxproj_path = File.join(xcodeproj_path, "project.pbxproj")
+
+      # given this file will not be modified, the quickest way I could think to assert that nothing changes
+      # is to just grab the offending lines out of the file and do a contains and not contains
+      nuke_line = line_from_file(18, pbxproj_path)
+      nuke_line_2 = line_from_file(135, pbxproj_path)
+      alamofire_line = line_from_file(19, pbxproj_path)
+      alamofire_line_2 = line_from_file(136, pbxproj_path)
+
+      file.runner.execute(:bump)
+
+      nuke_line_test = line_from_file(18, pbxproj_path)
+
+
+      expect(nuke_line).to include("Nuke in Frameworks")
+      expect(nuke_line_2).to include("Nuke in Frameworks")
+      expect(nuke_line).not_to include("BuildFile in Frameworks")
+
+      expect(alamofire_line).to include("Alamofire in Frameworks")
+      expect(alamofire_line_2).to include("Alamofire in Frameworks")
+      expect(alamofire_line).not_to include("BuildFile in Frameworks")
+    end
+
+    # it "should only adjust the specified schemes version" do 
+      
+    # end
+    
     
     after do
       cleanup_fixtures
